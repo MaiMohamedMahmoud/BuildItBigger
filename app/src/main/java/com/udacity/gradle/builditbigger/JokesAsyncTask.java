d6bf881094c745c18c03b48a14130e6206140a23
MaiMohamedMahmoud <mai.mohamed.mahmoud91@gmail.com>
2020-08-23 02:04:23 +0200

    ruuning on physical Device

--- app/src/main/java/com/udacity/gradle/builditbigger/JokesAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokesAsyncTask.java
@@ -19,7 +19,7 @@ class JokesAsyncTask extends AsyncTask<Void, Void, String> {
     private static MyApi myApiService = null;
     private JokesListener mJokesListener;
 
-    public JokesAsyncTask(JokesListener jokesListener){
+    public JokesAsyncTask(JokesListener jokesListener) {
         mJokesListener = jokesListener;
     }
 
@@ -30,21 +30,40 @@ class JokesAsyncTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... voids) {
-        if (myApiService == null) {  // Only do this once
+        if (myApiService == null) {
+//            // Only do this once
+//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+//                    new AndroidJsonFactory(), null)
+//                    // options for running against local devappserver
+//                    // - 10.0.2.2 is localhost's IP address in Android emulator
+//                    // - turn off compression when running against local devappserver
+//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                        @Override
+//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+//                            abstractGoogleClientRequest.setDisableGZipContent(true);
+//                        }
+//                    });
+//            // end options for devappserver
+
+            /**
+             * just change the rootUrl with your IpAddress to run the App on Physical Device
+             * to get your ipAddress
+             * for Windows:
+             * ipconfig
+             * for Mac/Linux:
+             * ifconfig
+             */
+
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://192.168.1.7:8081/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
                     });
-            // end options for devappserver
-
             myApiService = builder.build();
         }
 
